e9400b93a29f8f0f948bc7e9d5dac5bd2d546c00
bravf <bravfing@126.com>
2016-01-18 20:11:25 +0800

    init it

---
+++ main.js
@@ -0,0 +1,121 @@
+var fs = require('fs')
+var path = require('path')
+var http = require('http')
+var httpProxy = require('http-proxy')
+var $ = require('./jquery')
+var urlMod = require('url')
+
+function print(s){
+    var $logs = $('.logs')
+    if ($logs[0].scrollHeight > 1000){
+        $logs.html('')
+    }
+    $('.logs').append(s+'<br>')[0].scrollTop = 2000
+}
+
+print('logs...')
+
+//! 数据操作
+var DataOP = function (){
+    var currentPath = process.cwd()
+    var dataPath = path.join(currentPath, 'data.json')
+    var data = {urlMapping:{}}
+
+    if (fs.existsSync(dataPath)){
+        try {
+            data = JSON.parse(fs.readFileSync(dataPath))
+        }
+        catch (ex){
+        }
+    }
+
+    return {
+        getUrlMapping : function (){
+            return data.urlMapping
+        },
+        saveData : function (url1, url2, urlData){
+            if (url1 != ''){
+                delete data.urlMapping[url1]
+            }
+
+            if (url2 != ''){
+                data.urlMapping[url2] = {data:urlData}
+            }
+
+            try {
+                fs.writeFileSync(dataPath, JSON.stringify(data))
+            }
+            catch (ex){
+                print('文件写入错误！' + ex.toString())
+            }
+        }
+    }
+}()
+
+var Main = function (){
+    var urlMapping = DataOP.getUrlMapping()
+    var proxy = httpProxy.createProxyServer({})
+
+    var server = http.createServer(function (req, res){
+        var reqUrl = req.url
+        var reqPath = urlMod.parse(reqUrl).pathname
+
+        res.setHeader('X-Proxy-Header', 'small-proxy-server')
+
+        //如果配置了mock
+        if (reqPath in urlMapping){
+            print(`<b style="color:#4cae4c">${reqUrl}</b>`)
+            var customRes = urlMapping[reqPath]
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'application/json')
+            res.write(customRes.data)
+            res.end()
+        }
+        else {
+            print(reqUrl)
+            proxy.web(req, res, {
+                target : 'http://127.0.0.1:80'
+            })
+        }
+    })
+
+    return {
+        run : function (){
+            server.listen(8080)
+        }
+    }
+
+}()
+
+//Main.run()
+
+module.exports = $.extend(DataOP, Main)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
